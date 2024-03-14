fc2db1974e492e5b2497f71715263eb24da9c724
osakana0630 <osakana0630@gmail.com>
2024-03-14 17:16:02 +0900

    カードスケルトンのスタイル修正

--- components/pokemon-card-skeleton.tsx
+++ components/pokemon-card-skeleton.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export async function PokemonCardSkeleton() {
   return (
-    <Skeleton className="rounded-lg px-5 py-4 m-3 transition-colors dark:border-gray-500 hover:border-gray-300w-[270px] h-[300px]">
-      <Skeleton className="m-4 h-[200px] w-[200px] rounded-xl" />
+    <Skeleton className="rounded-lg px-5 py-4 m-3 transition-colors dark:border-gray-500 hover:border-gray-300">
+      <Skeleton className="m-4 h-[120px] w-[120px] rounded-xl" />
     </Skeleton>
   );
 }
